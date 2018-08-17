6fd5123d283c3b89a24105e0d3589a6305076c49
Pablo Anttila <fimbo@Pablos-MacBook-Pro.local>
2018-08-17 16:46:55 +0200

    Feat: project has new index view with portfolio -> works list

---
+++ pages/works.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import Link from 'next/link'
+import Layout from '../components/views/Layout'
+import sanity from '../lib/sanity'
+import styles from './styles/movies'
+import listStyles from './styles/list'
+import sanityClient from '../lib/sanity'
+import imageUrlBuilder from '@sanity/image-url'
+
+const imageBuilder = imageUrlBuilder(sanityClient)
+
+function imageUrlFor(source) {
+  return imageBuilder.image(source)
+}
+
+const query = `*[_type == "works"] {
+  _id,
+  title,
+  publishedAt,
+  "poster":   mainImage.asset->url,
+  // "posterAspect": poster.asset->.metadata.dimensions.aspectRatio,
+  // "director": crewMembers[job == "Director"][0].person->name
+}[0...50]
+`
+
+export default class Works extends React.Component {
+
+  static async getInitialProps() {
+    return {
+      works: await sanity.fetch(query)
+    }
+  }
+
+  render() {
+    const {works} = this.props
+    return (
+      <Layout>
+        <div className="movies">
+          <ul className="list">
+            {works.map(work => (
+              <li key={work._id} className="list__item">
+                <Link href={{pathname: '/work', query: {id: work._id}}}>
+                  <a>
+                  <h3>{work.title}</h3>
+
+                  </a>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <style jsx>{styles}</style>
+        <style jsx>{listStyles}</style>
+      </Layout>
+    )
+  }
+}
